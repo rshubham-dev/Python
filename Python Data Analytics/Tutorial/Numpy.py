1b50889a1b413743af3518bed2ab5f78c55d709e
rshubham-dev <shubhamkrg1819@gmail.com>
2024-08-18 13:11:45 +0530

    python for data analytics

---
+++ Python Data Analytics/Tutorial/Numpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = np.array([20,30,40])
+b = np.array([60,70,80])
+print(a)
+print(b)
--- Tutorial/Mega Project 1 - Jarvis/tempCodeRunnerFile.py
+++ Tutorial/Mega Project 1 - Jarvis/tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
+def speak(text):
+    engine.say(text)
+    # engine.save_to_file(text, 'test.mp3') # to save voice to a file
+    engine.runAndWait()
 
-engine.setProperty('rate', 220)     # setting up new voi
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}")
+        if r.status_code == 200:
+            data = r.json()
